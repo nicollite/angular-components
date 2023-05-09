e9b94dd8267e509512f2c5d70197a625ed8930f2
Nicolas Conte <nicolas.conte@alest.com.br>
2023-05-08 21:08:09 -0300

    change app

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,29 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AngInputModule } from './components/ang-input/ang-input.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { InputComponent } from './pages/input/input.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AngInputModule } from "./components/ang-input/ang-input.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NbThemeModule, NbLayoutModule } from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { ComponentViewerComponent } from "./pages/component-viewer/component-viewer.component";
+import { TesterComponent } from "./pages/tester/tester.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputComponent
-  ],
+  declarations: [AppComponent, ComponentViewerComponent, TesterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngInputModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
+    NbThemeModule.forRoot({ name: "default" }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // Component modules
+    AngInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
